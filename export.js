9ddaad69d937e6533c0b24dc65a8c22d86ec70c3
AndreiH96 <huzunandrei96@gmail.com>
2023-09-30 13:55:22 +0300

    First commit

---
+++ export.js
@@ -0,0 +1,8 @@
+import { exercitiu1 } from "./index.js";
+import { exercitiu2 } from "./index.js";
+import { exercitiu3 } from "./index.js";
+
+
+console.log(exercitiu1(3,2))
+console.log(exercitiu2( 'Huzun','Andrei'))
+console.log(exercitiu3('Andrei','Huzun','Masculin','27 Ani' ))
---
+++ index.js
@@ -0,0 +1,26 @@
+//ex1
+
+export const exercitiu1 = function(number1,number2){
+    return  number1 + number2
+}
+
+
+
+//ex2
+
+export const exercitiu2 = function (Nume, Prenume){
+    return Nume +' '+ Prenume
+}
+
+
+
+//ex3 
+
+export const exercitiu3 = function(Nume, Prenume,){
+    return arguments
+}
+
+
+
+
+
